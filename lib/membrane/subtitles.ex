4d74ad8561bd29c91c412f4fb6bb3838222e2e4d
Daniel Morandini <danielmorandini@me.com>
2025-09-25 12:33:58 +0200

    Initialize subtitles plugin

---
+++ lib/membrane/subtitles.ex
@@ -0,0 +1,18 @@
+defmodule Membrane.Subtitles do
+  @moduledoc """
+  Documentation for `Membrane.Subtitles`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Membrane.Subtitles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/membrane/subtitles_test.exs
@@ -0,0 +1,8 @@
+defmodule Membrane.SubtitlesTest do
+  use ExUnit.Case
+  doctest Membrane.Subtitles
+
+  test "greets the world" do
+    assert Membrane.Subtitles.hello() == :world
+  end
+end
