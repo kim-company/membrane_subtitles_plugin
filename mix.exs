1bfb21ef9a9731e700bbc58036e24346006d529a
Daniel Morandini <danielmorandini@me.com>
2025-12-11 16:15:42 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Membrane.Subtitles.MixProject do
   def project do
     [
       app: :membrane_subtitles_plugin,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
